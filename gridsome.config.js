9533a9cfa3b6c8133decbecb9b41fcebaf8929d5
香香鸡 <leimiliya@hotmail.com>
2021-10-31 14:21:00 +0800

    开发完成

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,44 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: '拉钩教育',
+  siteDescription: '大前端',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'BlogPost',
+        path: './content/blog/**/*.md',
+      },
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post', 'tag'],
+        singleTypes: ['general'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: 'xdxdji',
+        //   password: '123456',
+        // },
+      },
+    },
+  ],
+  templates: {
+    StrapiPost: [
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue',
+      },
+    ],
+    StrapiTag: [
+      {
+        path: '/tag/:id',
+        component: './src/templates/Tag.vue',
+      },
+    ],
+  },
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,119 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <!-- Page Header-->
+    <header
+      class="masthead"
+      :style="{
+        backgroundImage: `url(http://localhost:1337${general.cover.url})`
+      }"
+    >
+      <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+            <div class="site-heading">
+              <h1>{{general.title}}</h1>
+              <span class="subheading">{{general.sub}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Main Content-->
+    <div class="container px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <!-- Post preview-->
+          <div
+            class="post-preview"
+            v-for="edge in $page.posts.edges"
+            :key="edge.node.id"
+          >
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">{{edge.node.title}}</h2>
+              <!-- <h3
+                class="post-subtitle"
+              >Problems look mighty small from 150 miles up</h3>-->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">{{edge.node.user.username}}</a>
+              on {{edge.node.created_at}}
+            </p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <g-link :to="'/tag/'+tag.id">{{tag.title}}</g-link>&bsp;&bsp;
+              </span>
+            </p>
+            <hr class="my-4" />
+          </div>
+          <!-- Divider-->
+          <!-- Pager-->
+          <!-- <div class="d-flex justify-content-end mb-4">
+            <a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
+          </div>-->
+          <Pager :info="$page.posts.pageInfo" />
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
+<page-query>
+  query($page:Int) {
+  posts: allStrapiPost(perPage:1, page:$page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        created_at
+        tags {
+          id
+          title
+        }
+        user {
+          id
+          username
+        }
+      }
+    }
+  }
+
+  allStrapiGeneral {
+    edges {
+      node {
+        title
+        id
+        sub
+        cover {
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import { Pager } from 'gridsome'
 export default {
+  name: 'HomePage',
   metaInfo: {
     title: 'Hello, world!'
+  },
+  components: {
+    Pager
+  },
+  computed: {
+    general() {
+      return this.$page.allStrapiGeneral.edges[0].node
+    }
   }
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
-}
 </style>
